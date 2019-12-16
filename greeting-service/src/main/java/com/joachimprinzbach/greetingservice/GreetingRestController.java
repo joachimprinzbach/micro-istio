298bec90cb7f9eaa15c9a8f30b4f4c1b471cff8c
Nikolas Philips <nikolas.philips@gmail.com>
2019-12-16 22:38:46 +0000

    Make concat url configurable

--- greeting-service/src/main/java/com/joachimprinzbach/greetingservice/GreetingRestController.java
+++ greeting-service/src/main/java/com/joachimprinzbach/greetingservice/GreetingRestController.java
@@ -3,6 +3,7 @@ package com.joachimprinzbach.greetingservice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -15,10 +16,14 @@ public class GreetingRestController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${CONCAT_SERVICE_URL}")
+    private String concatServiceUrl;
+
     @GetMapping(path = "greeting")
     public GreetingDto greet() {
         LOGGER.info("Calling greeting");
-        String response = restTemplate.getForObject("http://concat-service-backend.concat-service.svc:8080/concat", String.class);
+
+        String response = restTemplate.getForObject(concatServiceUrl, String.class);
         LOGGER.info("Received value from concat service: " + response);
         return new GreetingDto("Hello " + response);
     }
