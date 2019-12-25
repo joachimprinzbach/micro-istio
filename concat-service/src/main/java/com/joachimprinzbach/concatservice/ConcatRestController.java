759735ceddb1eecc2ee8af3f17d41c6e123db8da
Nikolas Philips <nikolas.philips@gmail.com>
2019-12-25 04:21:14 +0100

    Add more debug

--- concat-service/src/main/java/com/joachimprinzbach/concatservice/ConcatRestController.java
+++ concat-service/src/main/java/com/joachimprinzbach/concatservice/ConcatRestController.java
@@ -2,6 +2,7 @@ package com.joachimprinzbach.concatservice;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +12,8 @@ public class ConcatRestController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ConcatRestController.class);
 
     @GetMapping(path = "concat")
-    public ResultDto concat() {
-        LOGGER.info("Calling concat");
+    public ResultDto concat(@CookieValue(value = "email", required = false, defaultValue = "unknown") String email) {
+        LOGGER.info("Calling concat with cookie email="+email);
         return new ResultDto("dummy");
     }
 }
