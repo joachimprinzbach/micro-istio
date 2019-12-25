1b9c27a98fd305d83be32e0d926fe2e8fc47ec5b
Nikolas Philips <nikolas.philips@gmail.com>
2019-12-25 02:59:40 +0100

    Set cookie

--- greeting-service/src/main/java/com/joachimprinzbach/greetingservice/GreetingRestController.java
+++ greeting-service/src/main/java/com/joachimprinzbach/greetingservice/GreetingRestController.java
@@ -5,13 +5,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class GreetingRestController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GreetingRestController.class);
+    private static final int MAX_AGE_SECONDS = 86400;
 
     @Autowired
     private RestTemplate restTemplate;
@@ -26,5 +32,23 @@ public class GreetingRestController {
         String response = restTemplate.getForObject(concatServiceUrl, String.class);
         LOGGER.info("Received value from concat service: " + response);
         return new GreetingDto("Hello " + response);
+    }
+
+    @RequestMapping(path = "login")
+    public String login(HttpServletRequest request, HttpServletResponse response) {
+        LOGGER.info("Calling login");
+        String email = request.getParameter("email");
+        Cookie cookie = createCookie("email", email);
+        response.addCookie(cookie);
+        return "Set email="+email;
+    }
+
+    private Cookie createCookie(String cookieName, String cookieValue) {
+        Cookie cookie = new Cookie(cookieName, cookieValue);
+        cookie.setPath("/");
+        cookie.setMaxAge(MAX_AGE_SECONDS);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        return cookie;
     }
 }
